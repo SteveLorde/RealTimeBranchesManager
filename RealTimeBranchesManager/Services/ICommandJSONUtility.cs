e8aea46bad0750c059eb7df8ff7880a396018b5e
SteveLorde <mostafa.maher98@gmail.com>
2025-01-21 19:01:11 +0200

    More work on ICommandJSONUtility.cs

--- RealTimeBranchesManager/Services/ICommandJSONUtility.cs
+++ RealTimeBranchesManager/Services/ICommandJSONUtility.cs
@@ -1,4 +1,9 @@
-﻿using RealTimeBranchesManager.Commands.Model;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using RealTimeBranchesManager.Commands.Model;
 
 namespace RealTimeBranchesManager.Services;
 
@@ -6,12 +11,17 @@ public static class ICommandJSONUtility
 {
 	public static string Serialize(ICommand command)
 	{
-		
+		return JsonSerializer.Serialize(command);
 	}
 
-	public static ICommand DeSerialize(string serializedCommand)
+	public static async Task<ICommand> DeSerialize(string serializedCommand)
 	{
-		ICommand command = null;
+		var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(serializedCommand));
+
+		var command = await JsonSerializer.DeserializeAsync<ICommand>(memoryStream);
+
+		if (command == null) throw new NullReferenceException("deserialized command is null");
+
 		return command;
 	}
 }
